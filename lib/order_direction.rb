aa2ac187387844490f197f64dfaa2758f75c3add
foundate <foundate@gmail.com>
2012-11-22 17:43:22 +0900

    OrderDirectionHelper require

--- lib/order_direction.rb
+++ lib/order_direction.rb
@@ -1,5 +1,6 @@
 require "order_direction/version"
 require "order_direction/order_direction"
+require "order_direction/order_direction_helper"
 
 # ActionController::Base.send :include, OrderDirection::ControllerExtension
 # ActiveRecord::Base.send :extend, OrderDirection::ModelExtension
